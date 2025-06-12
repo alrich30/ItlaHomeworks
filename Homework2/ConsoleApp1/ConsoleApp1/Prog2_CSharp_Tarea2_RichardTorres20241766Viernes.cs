bea956bc776afed2554fb6190e7b03fb5ac94f40
alrich30 <richardtorresperes@gmail.com>
2025-06-12 00:16:51 -0400

    Adding Homework #2

---
+++ Homework2/ConsoleApp1/ConsoleApp1/Prog2_CSharp_Tarea2_RichardTorres20241766Viernes.cs
@@ -0,0 +1,28 @@
+﻿class DetectorDeParesEImpares
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Ingrese un numero:");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero es par");
+            }
+            else
+            {
+                Console.WriteLine("El numero es impar");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Ingresaste un valor no numérico.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ocurrió un error inesperado: " + ex.Message);
+        }
+    }
+}
