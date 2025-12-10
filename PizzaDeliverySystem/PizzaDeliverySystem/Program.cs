d6d8e21cbed0a30a99ad4aa27a3c6d524570ff08
alrich30 <richardtorresperes@gmail.com>
2025-12-10 00:36:36 -0400

    Reapply "Merge pull request #2 from alrich30/develop"
    This reverts commit bf2d8cddeceb201dd4d7535c4498290e9166976a.

--- PizzaDeliverySystem/PizzaDeliverySystem/Program.cs
+++ PizzaDeliverySystem/PizzaDeliverySystem/Program.cs
@@ -4,8 +4,33 @@ using PizzaDeliverySystem.Application.Service;
 using PizzaDeliverySystem.Domain.Core.Repository;
 using PizzaDeliverySystem.Infrastructure.Context;
 using PizzaDeliverySystem.Infrastructure.Repositories;
+using PizzaDeliverySystem.Infrastructure.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var jwtKey = builder.Configuration["Jwt:Key"];
+var jwtIssuer = builder.Configuration["Jwt:Issuer"];
+var jwtAudience = builder.Configuration["Jwt:Audience"];
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtAudience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey!))
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 // 1) Controllers
 builder.Services.AddControllers();
@@ -25,11 +50,17 @@ builder.Services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<PizzaDbConte
 builder.Services.AddScoped<IPizzaRepository, PizzaRepository>();
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IIngredientRepository, IngredientRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 
 // 5) Servicios (Aplicación)
 builder.Services.AddScoped<IPizzaService, PizzaService>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IIngredientService, IngredientService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
 
 // 6) CORS
 //builder.Services.AddCors(options =>
@@ -53,6 +84,9 @@ var app = builder.Build();
 
 // Middlewares básicos
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
